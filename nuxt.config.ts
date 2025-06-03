7424cf14c9bddf666d000f04e6c79dc8d12f5197
Andy <andyclass50@hotmail.com>
2025-06-03 14:13:26 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   image: {
-    dir: 'assets/images',
-    provider: 'static'
+    dir: 'assets/images'
   },
   modules: [
     '@nuxt/content',
