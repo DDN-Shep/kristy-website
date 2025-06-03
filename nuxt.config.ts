60ecb76f1293da23d98181f6b94d527012503604
Andy <andyclass50@hotmail.com>
2025-06-03 13:34:17 +0100

    Netlify images not appearing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   image: {
+    dir: 'assets/images',
     provider: 'netlify'
   },
   modules: [
