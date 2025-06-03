92598543a7260f11b556b4d32256d6dea14baeed
Andy <andyclass50@hotmail.com>
2025-06-03 13:17:44 +0100

    Update nuxt.config.ts image fix - try again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css'],,
   image: {
-    dir: 'assets/images'
+    provider: 'netlify'
   },
   modules: [
     '@nuxt/content',
