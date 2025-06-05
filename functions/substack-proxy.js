611d6362bcc1478dbf0a23b7afa21bd8c733b9be
Andy <andyclass50@hotmail.com>
2025-06-05 12:44:27 +0100

    Update substack-proxy.js

--- functions/substack-proxy.js
+++ functions/substack-proxy.js
@@ -2,14 +2,19 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
     const apiUrl = 'https://kristinasheppard.substack.com/api/v1/archive?sort=new&offset=0&limit=14';
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET'
+    };
 
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
 
-        return { statusCode: 200, body: JSON.stringify(data) };
+        return { statusCode: 200, headers, body: JSON.stringify(data) };
     }
     catch (error) {
-        return { statusCode: 500, body: JSON.stringify({ error: 'Failed to fetch data' }) };
+        return { statusCode: 500, headers, body: JSON.stringify({ error: 'Failed to fetch data' }) };
     }
 };
